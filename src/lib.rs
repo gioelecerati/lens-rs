f616ffaf34cde2247386c4c0a1b619351af05846
gioelecerati <gioele@cerati.org>
2022-06-24 02:53:04 +0200

    rm wasm testing

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,11 @@ pub mod methods;
 pub mod tests;
 pub mod utils;
 
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
 #[derive(Debug, Clone)]
 pub enum Chain {
     Polygon,
 }
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 #[derive(Debug, Clone)]
 pub enum Net {
     Mumbai,
